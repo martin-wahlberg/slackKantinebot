4270f9a2c3597658452774db83e2455f59b763ea
Martin <martinwahlberg@icloud.com>
2020-07-24 15:58:26 +0200

    debug

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import app from './bolt';
 import actions from './Actions';
 import cronJobs from './CronJobs';
 
+console.log('her');
+console.log(process.env.SLACK_BOT_TOKEN);
+console.log(process.env.SLACK_SIGNING_SECRET);
+console.log('her');
 actions();
 cronJobs();
 
